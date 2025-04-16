3e4064494cd671769b994bc09f5ede80d57d3c97
Bridgerto <2740821@students.wits.ac.za>
2025-04-16 11:45:53 +0200

    Fixed syntax

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -3,4 +3,4 @@ console.log("hi");
 /*Let: keeps variables private. Allow for neatness vs  Var: keeps variable public/global. It has to to do with scoping variables*/
 
 //DOM Object
-window.alert("Hello fellow traveler!")
+window.alert("Hello fellow traveler!");
