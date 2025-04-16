eeeac98a1ca9d76341bb37797ef60c6d4f76c7ee
Bridgerto <2740821@students.wits.ac.za>
2025-04-16 11:45:34 +0200

    Testing DOM object

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -1,1 +1,6 @@
 console.log("hi");
+
+/*Let: keeps variables private. Allow for neatness vs  Var: keeps variable public/global. It has to to do with scoping variables*/
+
+//DOM Object
+window.alert("Hello fellow traveler!")
