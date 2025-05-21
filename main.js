12edb5e1b7591bb8fc8386b2a83d98996be538e4
Bridgerto <2740821@students.wits.ac.za>
2025-05-22 01:51:25 +0200

    Modular navigation js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 /*https://www.youtube.com/watch?v=M498DvQDkJo*/
+
+
+document.addEventListener("DOMContentLoaded", function()
+{
+    const burger = document.getElementById("burger");
+    const navLinks = document.getElementById("navLinks");
+      const overlay = document.getElementById("overlay");
+
+
+    burger.addEventListener("click", () => 
+    {
+        navLinks.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+
+    // When burger menu is dropped, you cannot scroll on the page
+    overlay.addEventListener("click", () => 
+    {
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+        document.body.classList.remove("noscroll");
+        document.getElementById("check").checked = false;
+    });
+
+});
