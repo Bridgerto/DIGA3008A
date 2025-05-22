3510c1c44b8ad06c95f3fd43eff63b34f2621f02
Bridgerto <2740821@students.wits.ac.za>
2025-05-22 03:24:19 +0200

    JS Return to top and close icon

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-/*https://www.youtube.com/watch?v=M498DvQDkJo*/
-
 
 document.addEventListener("DOMContentLoaded", function()
 {
     const burger = document.getElementById("burger");
-    const navLinks = document.getElementById("navLinks");
+      const close = document.getElementById("close");
+      const navLinks = document.getElementById("navLinks");
       const overlay = document.getElementById("overlay");
 
 
@@ -12,8 +11,16 @@ document.addEventListener("DOMContentLoaded", function()
     {
         navLinks.classList.toggle("active");
         overlay.classList.toggle("active");
+        document.body.classList.add("noscroll");
     });
 
+
+     close.addEventListener("click", () => 
+    {
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+        document.body.classList.remove("noscroll"); // Enable scroll
+    });
 
     // When burger menu is dropped, you cannot scroll on the page
     overlay.addEventListener("click", () => 
@@ -21,7 +28,29 @@ document.addEventListener("DOMContentLoaded", function()
         navLinks.classList.remove("active");
         overlay.classList.remove("active");
         document.body.classList.remove("noscroll");
-        document.getElementById("check").checked = false;
+    });
+
+    const toTopBtn = document.getElementById("toTopBtn");
+
+    window.addEventListener("scroll", () => 
+    {
+        const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        const pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        if ((scrollPosition / pageHeight) > 0.8) 
+        {
+            toTopBtn.style.display = "block";
+        } 
+        else 
+        {
+            toTopBtn.style.display = "none";
+        }
+    });
+
+    toTopBtn.addEventListener("click", () =>
+    {
+        window.scrollTo({ top: 0, behavior: "smooth" });
     });
 
 });
+
